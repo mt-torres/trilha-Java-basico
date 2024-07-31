11d2a4b2207d07a68413b1b3b679c5a562c827c0
mt-torres <mf_torres@yahoo.com.br>
2024-07-31 10:54:32 -0300

    Create the objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição Curso JavaScript");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp java developer");
+        bootcamp.setDescricao("Descrição Bootcamp java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMarcos = new Dev();
+        devMarcos.setNome("Marcos");
+        devMarcos.increverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devMarcos.getConteudosInscritos());
+        devMarcos.progredir();
+        System.out.println("Conteúdos concluidos: " + devMarcos.getConteudosConcluido());
+        System.out.println("Conteúdos inscritos: " + devMarcos.getConteudosInscritos());
+        System.out.println(devMarcos.calcularTotalXp());
+        System.out.println();
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.increverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        System.out.println("Conteúdos concluidos: " + devJoao.getConteudosConcluido());
+        System.out.println("Conteúdos inscritos: " + devJoao.getConteudosInscritos());
 
 
     }
